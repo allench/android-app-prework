2a33563deb8cd277df54edbd9c3fa9e7ee81fab5
allench <allench@yahoo-inc.com>
2015-07-22 19:40:33 +0800

    init: SimpleToDo

---
+++ app/src/main/java/com/example/allench/simpletodo/MainActivity.java
@@ -0,0 +1,125 @@
+package com.example.allench.simpletodo;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+public class MainActivity extends ActionBarActivity {
+    ArrayList<String> items;
+    ArrayAdapter<String> itemsAdapter;
+    ListView lvItems;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        readItems(); // load items from file
+
+        // init ListView
+        itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        lvItems = (ListView) findViewById(R.id.lvItems);
+        lvItems.setAdapter(itemsAdapter);
+
+        setupListViewListener(); // for click to remove item
+        setupEditTextListener(); // for enter to add item
+    }
+
+    // handle enter to add item
+    private void setupEditTextListener() {
+        final EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
+        etNewItem.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (keyCode == KeyEvent.KEYCODE_ENTER) {
+                    onAddItem(v);
+                }
+                return false;
+            }
+        });
+    }
+
+    // handle click to remove item
+    private void setupListViewListener() {
+        lvItems.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                items.remove(position);
+                itemsAdapter.notifyDataSetChanged();
+                writeItems();
+            }
+        });
+    }
+
+    // handle button to add item
+    public void onAddItem(View view) {
+        EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
+        String sInput = etNewItem.getText().toString().trim();
+        if (sInput.length() > 0) {
+            items.add(sInput);
+            itemsAdapter.notifyDataSetChanged();
+            etNewItem.setText("");
+            // save
+            writeItems();
+        }
+    }
+
+    // readItems from file
+    private void readItems() {
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+        try {
+            items = new ArrayList<String>(FileUtils.readLines(todoFile));
+        } catch (IOException e) {
+            items = new ArrayList<String>();
+        }
+    }
+
+    // writeItems to file
+    private void writeItems() {
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+        try {
+            FileUtils.writeLines(todoFile, items);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
